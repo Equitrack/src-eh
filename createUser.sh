6e6e28228d83503edcfce8df315e52b9ffd13cee
Equitrack <string3075@gmail.com>
2021-03-24 21:18:01 -0600

    Automatic change at: Wed Mar 24 09:18:01 PM CST 2021

--- createUser.sh
+++ createUser.sh
@@ -1,7 +1,11 @@
-# Create user
+#!/bin/bash
 
+# Variable
 nameUser="Alexander"
 
-mkdir /home/$nameUser
-useradd -d /home/$nameUser -s /bin/bash $nameUser
+# Create directory
+sudo mkdir /home/$nameUser
+
+# Create user and define directory
+sudo useradd -d /home/$nameUser -s /bin/bash $nameUser
 
