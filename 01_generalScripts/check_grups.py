fa703d9efbd571d045cf99b7df466c1678c3b183
Equitrack <string3075@gmail.com>
2021-03-26 16:22:01 -0600

    Automatic change at: Fri Mar 26 04:22:01 PM CST 2021

--- 01_generalScripts/check_grups.py
+++ 01_generalScripts/check_grups.py
@@ -3,6 +3,7 @@
 import requests
 
 token = "1420238693:AAG3X6JrQRd5TyrvV3_45mFLwgAIdyxXV6c"
+
 url = "https://api.telegram.org/bot" + token + "/getUpdates"
 
 response = requests.get(url)
@@ -11,5 +12,9 @@ response = requests.get(url)
 for i in response.headers:
     print(i + ":" + response.headers[i])
 
-print(response.status_code)
+
+if response.status_code == "200":
+    print(response.text)
+else:
+    print("Status code: " + response.status_code)
 
