410e49b0acc4ec847d0fda66e177630d6a3c2989
Equitrack <string3075@gmail.com>
2021-03-26 17:38:01 -0600

    Automatic change at: Fri Mar 26 05:38:01 PM CST 2021

--- 01_generalScripts/testParamSystem.py
+++ 01_generalScripts/testParamSystem.py
@@ -2,5 +2,5 @@
 import sys
 
 print("Número de parámetros: " + str(len(sys.argv)))
-print("Parámetro n: " + str(sys.argv))
+print("Parámetro n: " + str(sys.argv)[1])
 print("Contenido del parámetro n: ")
