db1948e23c5d158a428d4b6262cc14c19d681d49
Equitrack <string3075@gmail.com>
2021-03-26 17:08:02 -0600

    Automatic change at: Fri Mar 26 05:08:02 PM CST 2021

--- 01_generalScripts/smstelegram.py
+++ 01_generalScripts/smstelegram.py
@@ -2,11 +2,28 @@ import requests
 
 url = 'https://api.telegram.org/bot'
 
-jsonData = {'chat_id': '-595788453,',
+jsonData = {'chat_id': '-595788453',
             'text': 'MessageTest',
             'disable_notification': 'true'}
+
 header = {"Content-Type": "application/json"}
 
 #use the 'headers' parameter to set the HTTP headers:
 response = requests.post(url, jsonData, headers=header)
 
+# Muestra los headers de respuesta
+for i in response.headers:
+    print(i + ": " + response.headers[i])
+
+print("\n")
+
+if response.status_code == 200:
+    f = open("response.json", "w")
+    f.write(response.text)
+    f.close()
+   
+    # Pretty Printing JSON string back
+    print(json.dumps(json.loads(response.text), indent = 4, sort_keys=True))
+
+else:
+    print("status code: " + response.status_code)
