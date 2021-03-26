c1251d14c792c04172f1610f096a11574d9811cb
Equitrack <string3075@gmail.com>
2021-03-26 17:42:01 -0600

    Automatic change at: Fri Mar 26 05:42:01 PM CST 2021

--- 01_generalScripts/testParamSystem.py
+++ 01_generalScripts/testParamSystem.py
@@ -2,7 +2,7 @@
 import sys
 
 # verificar que haya parámetros
-if str(len(sys.argv)) == 1:
+if str(len(sys.argv)) == "1":
     print("Solo un parámetro")
 
 # print("Parámetro n: " + str(sys.argv[1]))
