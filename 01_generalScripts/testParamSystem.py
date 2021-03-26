e273e3218c578cf1df4175053f1f6de23a4cd676
Equitrack <string3075@gmail.com>
2021-03-26 17:50:01 -0600

    Automatic change at: Fri Mar 26 05:50:01 PM CST 2021

--- 01_generalScripts/testParamSystem.py
+++ 01_generalScripts/testParamSystem.py
@@ -3,9 +3,13 @@ import sys
 
 # verificar que haya parámetros
 if str(len(sys.argv)) == "1":
-    print("Uso: ./testParamSystem.py <option> <file/text>")
+    print("Use:")
+    print("./testParamSystem.py <option> <file/text>")
     print("Options:")
     print("\t-m\tSend message")
     print("\t-f\tRead file and send message")
+    print('Examples:')
+    print('\t./testParamSystem.py -m "Low disk space"')
+    print('\t./testParamSystem.py -f logs.txt')
 
 # print("Parámetro n: " + str(sys.argv[1]))
