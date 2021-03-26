08dbd83eedd1eec324e00c6f9c6c71113c4491ab
Equitrack <string3075@gmail.com>
2021-03-26 17:04:01 -0600

    Automatic change at: Fri Mar 26 05:04:01 PM CST 2021

--- 01_generalScripts/smstelegram.py
+++ 01_generalScripts/smstelegram.py
@@ -1,7 +1,8 @@
 import requests
 
 url = 'https://api.telegram.org/bot'
-jsonData = {'chat_id': '-1001298945859',
+
+jsonData = {'chat_id': '-595788453,',
             'text': 'MessageTest',
             'disable_notification': 'true'}
 header = {"Content-Type": "application/json"}
@@ -9,5 +10,3 @@ header = {"Content-Type": "application/json"}
 #use the 'headers' parameter to set the HTTP headers:
 response = requests.post(url, jsonData, headers=header)
 
-print(response)
-print(response.text)
