83d8298ba8911ed1059822ead483feba83c8c3e0
Equitrack <string3075@gmail.com>
2021-03-26 15:22:01 -0600

    Automatic change at: Fri Mar 26 03:22:01 PM CST 2021

--- 01_generalScripts/messageTelegram.sh
+++ 01_generalScripts/messageTelegram.sh
@@ -1,10 +1,12 @@
 API="https://api.telegram.org/bot"
 TOKEN="1420238693:AAG3X6JrQRd5TyrvV3_45mFLwgAIdyxXV6c"
-CHAT_ID="-1001298945859"
+CHAT_ID='"-1001298945859"'
 MESSAGE="MensajeDePrueba"
 
 jsonData="{"chat_id": $CHAT_ID,
 	"text": $MESSAGE,
 	"disable_notification": true}";
 
-curl -X POST -H "Content-Type: application/json" -d $jsonData "$API/$TOKEN/sendMessage"
+#curl -X POST -H "Content-Type: application/json" -d $jsonData "$API/$TOKEN/sendMessage"
+
+echo $jsonData
