9e1b117725503452a44a157874ed88bdda750795
Equitrack <string3075@gmail.com>
2021-03-26 17:48:01 -0600

    Automatic change at: Fri Mar 26 05:48:01 PM CST 2021

--- 01_generalScripts/testParamSystem.py
+++ 01_generalScripts/testParamSystem.py
@@ -3,9 +3,9 @@ import sys
 
 # verificar que haya parámetros
 if str(len(sys.argv)) == "1":
-    print("\tUso: ./testParamSystem.py <option> <file/text>")
-    print("\tOptions:")
-    print("\t\t-m\tSend message")
-    print("\t\t-f\tRead file and send message")
+    print("Uso: ./testParamSystem.py <option> <file/text>")
+    print("Options:")
+    print("\t-m\tSend message")
+    print("\t-f\tRead file and send message")
 
 # print("Parámetro n: " + str(sys.argv[1]))
