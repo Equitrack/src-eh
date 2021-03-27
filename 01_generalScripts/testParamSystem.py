8529cf8cdc606347a9a4791d1b211956c79c05b2
Equitrack <string3075@gmail.com>
2021-03-26 18:00:01 -0600

    Automatic change at: Fri Mar 26 06:00:01 PM CST 2021

--- 01_generalScripts/testParamSystem.py
+++ 01_generalScripts/testParamSystem.py
@@ -2,6 +2,8 @@
 import sys
 
 # verificar que haya parámetros
+print("Número de parámetros: "+ len(sys.argv))
+
 if str(len(sys.argv)) == "1":
     print("Use:")
     print("./testParamSystem.py <option> <file/text>")
@@ -11,9 +13,3 @@ if str(len(sys.argv)) == "1":
     print('Examples:')
     print('\t./testParamSystem.py -m "Low disk space"')
     print('\t./testParamSystem.py -f logs.txt')
-
-elif str(len(sys.argv)) == "3":
-    print(str(sys.argv[1]))
-
-else
-    print("else")
