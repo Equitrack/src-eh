fad573dbe6753de73ff7b12dd5181fec4158f807
Equitrack <string3075@gmail.com>
2021-03-30 19:30:01 -0600

    Automatic change at: Tue Mar 30 07:30:01 PM CST 2021

--- 01_generalScripts/capabilities.sh
+++ 01_generalScripts/capabilities.sh
@@ -5,12 +5,14 @@
 pathFile=`readlink -f $(which python3)`
 
 if [ -f "$pathFile" ]; then
-   if [[ `getcap $pathFile`| awk '{ print $2}' -eq 'cap_setuid=ep' ]]
-      echo "DELETE capability"
+   if [[ `getcap $pathFile | awk '{ print $2}'` -eq 'cap_setuid=ep' ]]; then
       setcap -r $pathFile
-   else
+      echo "DELETE capability"
+      exit 0
+   else:
       setcap cap_setuid+ep $pathFile
       $pathFile -c 'import os; os.setuid(0); os.system("/bin/bash")'
-else
-   echo "Python3 no existe"
+   fi
+else:
+   echo "Python3 no existe";
 fi
