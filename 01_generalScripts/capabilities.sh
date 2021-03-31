2d3d9f036813a149bba9d117c3f4ef2a476c71dd
Equitrack <string3075@gmail.com>
2021-03-30 19:12:01 -0600

    Automatic change at: Tue Mar 30 07:12:01 PM CST 2021

--- 01_generalScripts/capabilities.sh
+++ 01_generalScripts/capabilities.sh
@@ -5,11 +5,11 @@
 pathFile=`readlink -f $(which python3)`
 
 if [ -f "$pathFile" ]; then
-   echo "existe";
+   whoami
+
 else
    echo "Python3 no existe"
 fi
 
-#setcap cap_setuid+ep /usr/bin/$binary
 #echo "Status capability: $(getcap /usr/bin/$binary)"
 
