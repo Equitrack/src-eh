a0fee4f7fc6b23313d94538b20f9dbc02716a027
Equitrack <string3075@gmail.com>
2021-03-30 20:54:01 -0600

    Automatic change at: Tue Mar 30 08:54:01 PM CST 2021

--- 01_generalScripts/capabilities.sh
+++ 01_generalScripts/capabilities.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 # execute with root user
-
-getcap -r / 2>/dev/null
-
+# list capabilities getcap -r / 2>/dev/null
 pathFile=`readlink -f $(which python3)`
 if [ -f "$pathFile" ]; then
    response=`getcap $pathFile | awk '{ print $2}'`;
    if [[ $response == "cap_setuid=ep" ]] ; then
       setcap -r $pathFile;
       echo "DELETE capability";
-      exit 0;
    else
       echo "ADD capability"
       setcap cap_setuid+ep $pathFile
