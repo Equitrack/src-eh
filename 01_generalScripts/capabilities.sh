a911d7adbc17ddc2b38dffa8338850829114825d
Equitrack <string3075@gmail.com>
2021-03-30 19:00:01 -0600

    Automatic change at: Tue Mar 30 07:00:01 PM CST 2021

--- 01_generalScripts/capabilities.sh
+++ 01_generalScripts/capabilities.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# execute with root user
+
 binary=`python3 --version | sed 's/  *//g' | cut -d '.' -f 1,2`
 setcap cap_setuid+ep /usr/bin/$binary
+
+echo "Status capability: $(getcap /usr/bin/$binary)"
 
