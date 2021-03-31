6dce084003ea0a29c66dc40f100472a58c9b7211
Equitrack <string3075@gmail.com>
2021-03-30 18:56:01 -0600

    Automatic change at: Tue Mar 30 06:56:01 PM CST 2021

--- 01_generalScripts/capabilities.sh
+++ 01_generalScripts/capabilities.sh
@@ -1,1 +1,5 @@
-setcap cap_setuid(0)+ep /usr/bin
+#!/bin/bash
+
+echo `python3 --version | sed 's/  *//g' | cut -d '.' -f 1,2`
+setcap cap_setuid(0)+ep /usr/bin/$(python3 --version | sed 's/  *//g' | cut -d '.' -f 1,2)
+
