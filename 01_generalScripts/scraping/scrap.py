5a2d0e71dde117ce1fe5fff3e89b4d33c901f7cb
Equitrack <string3075@gmail.com>
2021-04-03 12:04:01 -0600

    Automatic change at: sáb 03 abr 2021 12:04:01 CST

--- 01_generalScripts/scraping/scrap.py
+++ 01_generalScripts/scraping/scrap.py
@@ -4,7 +4,9 @@ import os
 import sys
 
 def error_use():
-    print("Used: " + sys.argv[0])
+    print("Used: " + sys.argv[0] + " <url> " + "<words>")
+    print("\tExample:\n")
+    print(sys.argv[0] + " https://supermex.com.mx {2060, 2070, 2080}")
 
 if __name__ == "__main__":
     print(len(sys.argv))
