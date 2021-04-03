156b7d381395b8f06ed860c254078c6051fb783f
Equitrack <string3075@gmail.com>
2021-04-03 12:02:01 -0600

    Automatic change at: sáb 03 abr 2021 12:02:01 CST

--- 01_generalScripts/scraping/scrap.py
+++ 01_generalScripts/scraping/scrap.py
@@ -3,16 +3,14 @@
 import os
 import sys
 
-
 def error_use():
     print("Used: " + sys.argv[0])
 
 if __name__ == "__main__":
     print(len(sys.argv))
     if(len(sys.argv) == 2):
-        print("Dos argumentos")
+        print("Continue..."
     else:
-        print("Diferente de dos argumentos")
         error_use();
 
 
