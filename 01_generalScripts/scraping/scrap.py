6f5ad84c3464e7a4b44c5c82eb93eaa5065d8e79
Equitrack <string3075@gmail.com>
2021-04-03 12:10:01 -0600

    Automatic change at: sáb 03 abr 2021 12:10:01 CST

--- 01_generalScripts/scraping/scrap.py
+++ 01_generalScripts/scraping/scrap.py
@@ -5,12 +5,11 @@ import sys
 import requests
 
 def error_use():
-    print("Used: " + sys.argv[0] + " <url> " + "<words>")
-    print("\tExample:\n")
-    print(sys.argv[0] + " https://supermex.com.mx {2060, 2070, 2080}")
+    print("Used: " + sys.argv[0] + " <url> " + "<words> \n")
+    print("\tExample:")
+    print("\t" + sys.argv[0] + " https://supermex.com.mx {2060, 2070, 2080}")
 
 if __name__ == "__main__":
-    print(len(sys.argv))
     if(len(sys.argv) == 3):
         response = requests.get(sys.argv[1])
         if response.status_code == 200:
